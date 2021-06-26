8a840c5012a97c288ddef463aaf31e6b26e438ab
chayezo <yejinchaa@naver.com>
2021-06-26 20:15:44 +0900

    0626 done

--- toy/09_power.js
+++ toy/09_power.js
@@ -45,3 +45,15 @@ function power(base, exponent) {
     if (exponent % 2 === 1) return (base * result) % 94906249;
     else return result;
 }
+
+// 그냥...내코드...
+function power(base, exponent) {
+    if (exponent === 0) return 1;
+    let half = power(base, Math.floor(exponent / 2));
+    let result = half * half;
+    if (exponent % 2 === 0) {
+        return result;
+    } else {
+        return base * result;
+    }
+}
