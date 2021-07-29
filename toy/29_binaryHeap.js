325d1ea1f49ab906f1b7e292cce05922124cea4f
chayezo <yejinchaa@naver.com>
2021-07-29 23:20:49 +0900

    Thu Jul 29 : modify 29_binaryHeap

--- toy/29_binaryHeap.js
+++ toy/29_binaryHeap.js
@@ -4,22 +4,7 @@
  */
 // 아래 코드는 수정하지 마세요.
 function swap(idx1, idx2, arr) {
-    // 두 변수를 바꾸는 방법
-
-    // 1) 임시 변수를 활용한 방법
-    // let temp = arr[idx1];
-    // arr[idx1] = arr[idx2];
-    // arr[idx2] = temp;
-
-    // 2) Destructuring assignment를 활용한 방법
-    // arr이 reference type이라 가능
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-
-    // 3) XOR 연산을 활용한 방법
-    // arr이 reference type이라 가능
-    // arr[idx1] ^= arr[idx2];
-    // arr[idx2] ^= arr[idx1];
-    // arr[idx1] ^= arr[idx2];
 }
 // 들어오는 idx의 부모 인덱스 구하기
 function getParentIdx(idx) {
